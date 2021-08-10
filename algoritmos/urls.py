d996e62a91e0c203a52f87a228f1779862d9b65c
Marco Molina <marco@hotmail.com>
2021-08-09 20:00:23 -0500

    Implementación EDA

--- algoritmos/urls.py
+++ algoritmos/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.GUI, name='GUI'),
+
     path('EDA/', views.EDA, name='EDA'),
+    path('EDA/numericas', views.EDAnumericas, name='EDAnumericas'),
+    path('EDA/nominales', views.EDAnominales, name='EDAnominales'),
+    
     path('PCA/', views.PCA, name='PCA'),
     path('Clustering/', views.Clustering, name='Clustering'),
     path('Regresion/', views.Regresion, name='Regresion'),
--- algoritmos/views.py
+++ algoritmos/views.py
@@ -2,32 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files import File
 
-#AES ECB y AES CBC
-from Cryptodome.Cipher import AES
-#SHA-2 384
-from Cryptodome.Hash import SHA384
-#SHA-2 512
-from Cryptodome.Hash import SHA512
-#SHA-3 384
-from Cryptodome.Hash import SHA3_384
-#SHA-3 512
-from Cryptodome.Hash import SHA3_512
-#RSA OAEP y PSS
-from Cryptodome.PublicKey import RSA
-from Cryptodome.Cipher import PKCS1_OAEP
-from Cryptodome.Signature import pkcs1_15
-from Cryptodome.Signature import pss
-#DSA Y ECDSA
-from Cryptodome.PublicKey import DSA
-from Cryptodome.PublicKey import ECC
-from Cryptodome.Signature import DSS
-from Cryptodome.Hash import SHA256
-#Para estadística de los resultados y tiempos
-import statistics
+import pandas as pd # Manipulación y análisi de datos
+import numpy as np # Para crear vectores matrices de n dimensiones
+import matplotlib.pyplot as plt # Para genenar gráficos
+import seaborn as sns # Para vasialización de datos
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 
 from time import time
 from pathlib import Path
 import os
+import io
+import urllib
+import base64
+import json
 
 
 
@@ -77,216 +67,136 @@ def GUI(request):
 def EDA(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
     x = os.path.join(BASE_DIR, 'files')
+    dataEDA = pd.read_csv(x+'/melb_data.csv')
 
-    listd=[]#Lista del parametro d
-    listQX=[]#Lista del punto X
-    listQY=[]#Lista del punto Y
-    listMsg=[]#Lista de los mensajes
-    listR=[]#Lista de firmas R
-    listS=[]#Lista de firmas S
+    headTen = dataEDA.head(10) # Regresa los prieros 10 valores
+    headTenHTML = headTen.to_html()
+    datosFaltantes = dataEDA.isnull().sum() # Identificación de datos faltantes.
+    datosFaltantesDic = datosFaltantes.to_dict()
+    datatype = dataEDA.dtypes
+    datatypeDic = datatype.to_dict()
+    dataCorr = dataEDA.corr()
+    dataCorrHTML = dataCorr.to_html()
 
-    archivo = open(x+"\ECC_P521_GEN.txt","r")
+    plt.figure(figsize=(14,7))
+    sns.heatmap(dataEDA.corr(), cmap='RdBu_r', annot=True)
     
-    for linea in archivo.readlines():
-        if "Qx = " in linea:#Lee el punto X del archivo
-            fQx = linea.lstrip("Qx = ")
-            listQX.append(fQx.rstrip("\n"))
-        elif "Qy = " in linea:#Lee el punto Y del archivo
-            fQy = linea.lstrip("Qy = ")
-            listQY.append(fQy.rstrip("\n"))
-        elif "Msg = " in linea:#Lee los mensajes del archivo
-            m = linea.lstrip("Msg = ")
-            listMsg.append(m.rstrip("\n"))
-        elif "R = " in linea:#Lee la firma R del archivo
-            fr = linea.lstrip("R = ")
-            listR.append(fr.rstrip("\n"))
-        elif "S = " in linea:#Lee la firma s del archivo
-            fs = linea.lstrip("S = ")
-            listS.append(fs.rstrip("\n"))
-        elif "d = " in linea:#Lee el parametro Y del archivo
-            fd = linea.lstrip("d = ")
-            listd.append(fd.rstrip("\n"))
-    archivo.close()
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    correlaciones = 'data:image/png;base64,' + urllib.parse.quote(string)
+    
     context = {
-        'timeGenECC_521': 'timeGenECC_521',
-        'timeVerECC_521': 'timeVerECC_521',
-        'timeGenECC_384': 'timeGenECC_384',
-        'timeVerECC_384': 'timeVerECC_384',
-        'listvector': 'listvector',
-        'timelist':'timelist'
+        'headTenHTML' : headTenHTML,
+        'datosFaltantesDic': datosFaltantesDic,
+        'datatypeDic': datatypeDic,
+        'dataCorrHTML': dataCorrHTML,
+        'correlaciones':correlaciones
     }
     return render(request, 'EDA.html', context)
 
-
+def EDAnumericas(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
     x = os.path.join(BASE_DIR, 'files')
+    DatosVac = pd.read_csv(x+'/melb_data.csv')
 
-    listd=[]#Lista del parametro d
-    listQX=[]#Lista del punto X
-    listQY=[]#Lista del punto Y
-    listMsg=[]#Lista de los mensajes
-    listR=[]#Lista de firmas R
-    listS=[]#Lista de firmas S
+    # Características numéricas
+    DatosVac.hist(figsize=(14,14), xrot=45)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    CaracNumericas = 'data:image/png;base64,' + urllib.parse.quote(string)
 
-    archivo = open(x+"\ECC_P521_GEN.txt","r")
-    
-    for linea in archivo.readlines():
-        if "Qx = " in linea:#Lee el punto X del archivo
-            fQx = linea.lstrip("Qx = ")
-            listQX.append(fQx.rstrip("\n"))
-        elif "Qy = " in linea:#Lee el punto Y del archivo
-            fQy = linea.lstrip("Qy = ")
-            listQY.append(fQy.rstrip("\n"))
-        elif "Msg = " in linea:#Lee los mensajes del archivo
-            m = linea.lstrip("Msg = ")
-            listMsg.append(m.rstrip("\n"))
-        elif "R = " in linea:#Lee la firma R del archivo
-            fr = linea.lstrip("R = ")
-            listR.append(fr.rstrip("\n"))
-        elif "S = " in linea:#Lee la firma s del archivo
-            fs = linea.lstrip("S = ")
-            listS.append(fs.rstrip("\n"))
-        elif "d = " in linea:#Lee el parametro Y del archivo
-            fd = linea.lstrip("d = ")
-            listd.append(fd.rstrip("\n"))
-    archivo.close()
     context = {
-        'timeGenECC_521': 'timeGenECC_521',
-        'timeVerECC_521': 'timeVerECC_521',
-        'timeGenECC_384': 'timeGenECC_384',
-        'timeVerECC_384': 'timeVerECC_384',
-        'listvector': 'listvector',
-        'timelist':'timelist'
+        'CaracNumericas' : CaracNumericas
     }
+    plt.switch_backend('agg')
+    return render(request, 'EDAnumericas.html', context)
 
-    return render(request, 'SHA.html', context)
+def EDAnominales(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    x = os.path.join(BASE_DIR, 'files')
+    DatosVac = pd.read_csv(x+'/melb_data.csv')
+
+    # Características nominales
+    for col in DatosVac.select_dtypes(include='object'):
+        if DatosVac[col].nunique() < 26:
+            sns.countplot(y=col, data=DatosVac)
+    
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    CaracNominales = 'data:image/png;base64,' + urllib.parse.quote(string)
+
+    context = {
+        'CaracNominales': CaracNominales
+    }
+    plt.switch_backend('agg')
+    return render(request, 'EDAnominales.html', context)   
 
 def PCA(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
     x = os.path.join(BASE_DIR, 'files')
+    DatosVac = pd.read_csv(x+'/country_vaccinations.csv')
+    DatosVac.hist(figsize=(14,14), xrot=45)
 
-    listd=[]#Lista del parametro d
-    listQX=[]#Lista del punto X
-    listQY=[]#Lista del punto Y
-    listMsg=[]#Lista de los mensajes
-    listR=[]#Lista de firmas R
-    listS=[]#Lista de firmas S
+    DatosVacHTML = DatosVac.to_html()
 
-    archivo = open(x+"\ECC_P521_GEN.txt","r")
-    
-    for linea in archivo.readlines():
-        if "Qx = " in linea:#Lee el punto X del archivo
-            fQx = linea.lstrip("Qx = ")
-            listQX.append(fQx.rstrip("\n"))
-        elif "Qy = " in linea:#Lee el punto Y del archivo
-            fQy = linea.lstrip("Qy = ")
-            listQY.append(fQy.rstrip("\n"))
-        elif "Msg = " in linea:#Lee los mensajes del archivo
-            m = linea.lstrip("Msg = ")
-            listMsg.append(m.rstrip("\n"))
-        elif "R = " in linea:#Lee la firma R del archivo
-            fr = linea.lstrip("R = ")
-            listR.append(fr.rstrip("\n"))
-        elif "S = " in linea:#Lee la firma s del archivo
-            fs = linea.lstrip("S = ")
-            listS.append(fs.rstrip("\n"))
-        elif "d = " in linea:#Lee el parametro Y del archivo
-            fd = linea.lstrip("d = ")
-            listd.append(fd.rstrip("\n"))
-    archivo.close()
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    image = 'data:image/png;base64,' + urllib.parse.quote(string)
+
     context = {
-        'timeGenECC_521': 'timeGenECC_521',
-        'timeVerECC_521': 'timeVerECC_521',
-        'timeGenECC_384': 'timeGenECC_384',
-        'timeVerECC_384': 'timeVerECC_384',
-        'listvector': 'listvector',
-        'timelist':'timelist'
+        'DatosVac': DatosVac,
+        'DatosVacHTML': DatosVacHTML,
+        'figure' : image
     }
     return render(request, 'PCA.html', context)
 
 def Clustering(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
     x = os.path.join(BASE_DIR, 'files')
+    DatosVac = pd.read_csv(x+'/country_vaccinations.csv')
+    DatosVac.hist(figsize=(14,14), xrot=45)
 
-    listd=[]#Lista del parametro d
-    listQX=[]#Lista del punto X
-    listQY=[]#Lista del punto Y
-    listMsg=[]#Lista de los mensajes
-    listR=[]#Lista de firmas R
-    listS=[]#Lista de firmas S
+    DatosVacHTML = DatosVac.to_html()
 
-    archivo = open(x+"\ECC_P521_GEN.txt","r")
-    
-    for linea in archivo.readlines():
-        if "Qx = " in linea:#Lee el punto X del archivo
-            fQx = linea.lstrip("Qx = ")
-            listQX.append(fQx.rstrip("\n"))
-        elif "Qy = " in linea:#Lee el punto Y del archivo
-            fQy = linea.lstrip("Qy = ")
-            listQY.append(fQy.rstrip("\n"))
-        elif "Msg = " in linea:#Lee los mensajes del archivo
-            m = linea.lstrip("Msg = ")
-            listMsg.append(m.rstrip("\n"))
-        elif "R = " in linea:#Lee la firma R del archivo
-            fr = linea.lstrip("R = ")
-            listR.append(fr.rstrip("\n"))
-        elif "S = " in linea:#Lee la firma s del archivo
-            fs = linea.lstrip("S = ")
-            listS.append(fs.rstrip("\n"))
-        elif "d = " in linea:#Lee el parametro Y del archivo
-            fd = linea.lstrip("d = ")
-            listd.append(fd.rstrip("\n"))
-    archivo.close()
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    image = 'data:image/png;base64,' + urllib.parse.quote(string)
+
     context = {
-        'timeGenECC_521': 'timeGenECC_521',
-        'timeVerECC_521': 'timeVerECC_521',
-        'timeGenECC_384': 'timeGenECC_384',
-        'timeVerECC_384': 'timeVerECC_384',
-        'listvector': 'listvector',
-        'timelist':'timelist'
+        'DatosVac': DatosVac,
+        'DatosVacHTML': DatosVacHTML,
+        'figure' : image
     }
     return render(request, 'Clustering.html', context)
 
 def Regresion(request):
     BASE_DIR = Path(__file__).resolve().parent.parent
     x = os.path.join(BASE_DIR, 'files')
+    DatosVac = pd.read_csv(x+'/country_vaccinations.csv')
+    DatosVac.hist(figsize=(14,14), xrot=45)
 
-    listd=[]#Lista del parametro d
-    listQX=[]#Lista del punto X
-    listQY=[]#Lista del punto Y
-    listMsg=[]#Lista de los mensajes
-    listR=[]#Lista de firmas R
-    listS=[]#Lista de firmas S
+    DatosVacHTML = DatosVac.to_html()
 
-    archivo = open(x+"\ECC_P521_GEN.txt","r")
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    image = 'data:image/png;base64,' + urllib.parse.quote(string)
 
-    for linea in archivo.readlines():
-        if "Qx = " in linea:#Lee el punto X del archivo
-            fQx = linea.lstrip("Qx = ")
-            listQX.append(fQx.rstrip("\n"))
-        elif "Qy = " in linea:#Lee el punto Y del archivo
-            fQy = linea.lstrip("Qy = ")
-            listQY.append(fQy.rstrip("\n"))
-        elif "Msg = " in linea:#Lee los mensajes del archivo
-            m = linea.lstrip("Msg = ")
-            listMsg.append(m.rstrip("\n"))
-        elif "R = " in linea:#Lee la firma R del archivo
-            fr = linea.lstrip("R = ")
-            listR.append(fr.rstrip("\n"))
-        elif "S = " in linea:#Lee la firma s del archivo
-            fs = linea.lstrip("S = ")
-            listS.append(fs.rstrip("\n"))
-        elif "d = " in linea:#Lee el parametro Y del archivo
-            fd = linea.lstrip("d = ")
-            listd.append(fd.rstrip("\n"))
-    archivo.close()
     context = {
-        'timeGenECC_521': 'timeGenECC_521',
-        'timeVerECC_521': 'timeVerECC_521',
-        'timeGenECC_384': 'timeGenECC_384',
-        'timeVerECC_384': 'timeVerECC_384',
-        'listvector': 'listvector',
-        'timelist':'timelist'
+        'DatosVac': DatosVac,
+        'DatosVacHTML': DatosVacHTML,
+        'figure' : image
     }
 
     return render(request, 'Regresion.html', context)
